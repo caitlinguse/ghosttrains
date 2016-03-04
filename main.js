fc2d359841b934892fefc1662e4a4db2c8e5d351
caitlinguse <caitlin.guse@mail.utoronto.ca>
2016-03-04 00:00:20 -0500

    Uploading initial files

---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FpdGxpbmd1c2UiLCJhIjoiY2lqbm4zZTNuMDBvM3VmbTVjM2M3eWxkayJ9.FodgOirhTvC5b_-woC5MTg';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v8',
+  center: [-79.9, 44.40],
+  zoom: 6.0
+});
